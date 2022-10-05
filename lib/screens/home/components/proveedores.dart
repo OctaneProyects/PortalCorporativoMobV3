7ba6c1c6516316b941ef30e9573b41c1a1d2e418
ealvarado <ealvarado@octanesystems.mx>
2022-10-05 16:29:17 -0700

    se sube nueva interfaz
    se usan cards para mostrar tajetas

--- lib/screens/home/components/proveedores.dart
+++ lib/screens/home/components/proveedores.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../../../providers/usuraio_providers.dart';
+import 'package:portal_corp_v2/constants.dart';
+
 
 class Proveedores extends StatelessWidget{
   const Proveedores({super.key, required this.noemp});
@@ -16,12 +19,12 @@ final int noemp;
         ], 
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Perfil',
+          title: 'Poveedores',
           theme: ThemeData(
             primarySwatch:  Colors.blueGrey,
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
-          home:  MyVendors(title: 'Perfil', user: noemp),
+          home:  MyVendors(title: 'Poveedores', user: noemp),
         )
         
       
@@ -83,9 +86,21 @@ class _MyVendorsState extends State<MyVendors> {
                 ]),
       ),
       body: ListView(
+        
         children: [Column(
-          children: const [          
-            ElevatedCardExample()
+          children: const [  
+            SizedBox(
+                height: 20,
+              ),
+                    
+            CardProveedores(), 
+            SizedBox(
+                height: 20,
+              ),
+            CardProveedores(),
+            SizedBox(
+                height: 20,
+              ),
           ],
         )],
       ) 
@@ -95,31 +110,95 @@ class _MyVendorsState extends State<MyVendors> {
   }
 }
 
-class ElevatedCardExample extends StatelessWidget {
-  const ElevatedCardExample({super.key});
+class CardProveedores extends StatelessWidget {
+  const CardProveedores({super.key});
 
 final prov = 'Servicios SA.CV';
 final idProveedor = 'PG01234';
 final rfc = 'AAAE941110ku6';
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Card(
-        child: SizedBox(
-          width: 95,
-          height: 300,
-          child:  Center(
-            child: Column(
-              children:  [
-                Text('Proveedor: $prov'),
-                 Text('IdProveedor: $idProveedor'),
-                  Text('RFC: $rfc')
+    return 
 
-            ]),
-          ) ,
+      Card(
+        margin: const EdgeInsets.fromLTRB(20.0, 0, 20, 0),
+        //shadowColor: Colors.red,
+        elevation: 10,
+      child: Container(
+        height: 150,
+        color: Colors.white,
+        child: Row(
+          children:  [
+            const Center(
+              child:  Padding(
+                padding: EdgeInsets.all(20),
+                child: Expanded(
+                  child: 
+                   Icon(
+                           Icons.add_reaction_rounded,
+                           color: Color.fromARGB(255, 2, 115, 146),
+                           size: 30.0                  
+                      ),                      
+                ),
+              ), 
+            ),
+            Expanded(
+              child:Container(                
+                alignment: Alignment.center,
+                child:  Column(
+                  children:  [
+                     Expanded(
+                          flex: 10,
+                          child:  Column(
+                            
+                            children:  const [
+                               Text('Notificacion de: Aprobacion'),
+                               Text('Modulo: Aprbacion de proveedores'),
+                               Text('Proveedor: ECO DIRECCION PROFESIONAL '),
+                               Text('IdProveedor: PG1001 '),
+                               Text('RFC: EDP021217MSA '),
+                            ],
+
+                          )
+                        //   ListTile(
+                        //     title: Text("Proveedor"),
+                        //     subtitle: Text("IdProveedor"),
+                        //     trailing:Text("RFC"),
+                        //   ),
+                         ),
+                         Expanded(
+                          flex: 5,
+                          child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                          children: [TextButton(
+                            style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+                            child: const Text('Rechazar', style:  TextStyle( color: NowUIColors.white)),
+                            onPressed: (){}, 
+                            ),
+                           const SizedBox(width: 8,),
+                            TextButton(
+                              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),                             
+                            child: const Text('Aprobar', style:  TextStyle( color: Colors.white)),
+                            onPressed: (){}, 
+                            ),
+                            const SizedBox(width: 8,),
+                            TextButton(
+                             style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey)),
+                              
+                                child: const Text("Ver detalles", style:  TextStyle( color: Colors.white)),                                
+                                onPressed: (){},
+                              ),
+                              const SizedBox(width: 8,)
+
+                          ],
+                          )
+                          )                        
+                  ],                  
+                ),
+                )              
+            )            
+          ],
         ),
-        ),
-      );
-    
+      ),
+    );        
   }
 }
